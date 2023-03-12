5029caf6d9dd4f9acf23ff18bc095bb97a4cf336
ZhackerRoot <57174638+ZhackerRoot@users.noreply.github.com>
2023-03-12 19:18:39 +0500

    Debug False

--- Pro/settings.py
+++ Pro/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!mc1ykhpwo!@yy989!_!*=@zch(fx0nspf*07q4)htgu&69i_5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.railway.app']
 
 
 # Application definition
